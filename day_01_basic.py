a6b685782ebe769732c0b50fb302556172e6dad4
Jay prakash yadav <jayprakashyadav75597@gmail.com>
2026-01-31 17:51:11 +0530

    Add files via upload

---
+++ day_01_basic.py
@@ -0,0 +1,43 @@
+# Topic: Python
+"""
+Definition:
+Python is an interpreted  programming language used to build programs, automate tasks, and work with data easily.
+
+"""
+# my first program 
+
+print("Hello World!")
+
+Welcome="Hello world!"
+
+print(Welcome)
+
+# Topic: Comments in python
+"""
+Definition:
+> Comments in python are something that are ignored by the python interpreter.
+"""
+# This is a single line comment
+"""
+This is a multi line comment (docstring)
+"""
+
+# Topic: Variables in python 
+"""
+Definition:
+> Variables are used to store data in python
+> You can write anything as a variable name 
+       Eg. Name = "Jayprakash"
+           Age  = 21
+"""
+name="Jayprakash"
+age=21
+print("Name:", name)
+print("Age:", age)
+
+# Naming Convention 
+"""we can write variables in 3 ways
+ 1 Camel case ->   jayPrakash
+ 2 pascel case ->  JayPrakash
+ 3 Snake case ->   jay_prakash
+ """
